6dca5f9d28184a23e46758f60ed4c4f200ab76ba
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-08-04 20:12:42 +0500

    type fixing

--- src/components/component/pantry.tsx
+++ src/components/component/pantry.tsx
@@ -124,7 +124,7 @@ export function Pantry() {
       const file = e.target.files[0];
       const reader = new FileReader();
       reader.onloadend = () => {
-        setNewItem({ ...newItem, image: reader.result });
+        setNewItem({ ...newItem, image: reader.result as string });
       };
       if (file) {
         reader.readAsDataURL(file);
